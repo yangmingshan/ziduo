60c150c241d7bb23279a1498ce3cea06313a3f5f
Yang Mingshan <y.mingshan3@gmail.com>
2022-08-15 00:04:08 +0800

    feat: about

---
+++ src/pages/about/index.ts
@@ -0,0 +1,3 @@
+import { defineComponent } from '@vue-mini/wechat';
+
+defineComponent({});
--- src/pages/mine/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent, ref } from '@vue-mini/wechat';
-
-defineComponent(() => {
-  const greeting = ref('Have a fun journey');
-
-  return {
-    greeting,
-  };
-});
