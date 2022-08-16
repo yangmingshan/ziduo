882a8ecc224ebd8e992eab41e495e0095b027d8f
Yang Mingshan <y.mingshan3@gmail.com>
2022-08-16 21:37:11 +0800

    fix: android issue

--- src/logic/check.ts
+++ src/logic/check.ts
@@ -1,12 +1,4 @@
-import { WORD_LENGTH } from './constants';
 import { getIdiom } from './idioms';
-
-export function filterNonChineseChars(input: string) {
-  return [...input]
-    .filter((i) => /\p{Script=Han}/u.test(i))
-    .slice(0, WORD_LENGTH)
-    .join('');
-}
 
 export function checkValidIdiom(word: string, strict = false) {
   if (!strict) return true;
